4d6fec74584f1ba779e3cd96a786929ad3d0742d
roknikrol <roknikrol@gmail.com>
2020-01-29 00:45:28 +0300

    Git_hub_api

--- Lesson_1/Lesson_1_Git_hub_API.py
+++ Lesson_1/Lesson_1_Git_hub_API.py
@@ -1,8 +1,13 @@
 import requests
-
+import json
 main_link = 'https://api.github.com/users/'
 input_username = input('Git user_name:') # try my user name - roknikrol
 get_link = requests.get(main_link+input_username+'/repos')
 
-for repos in get_link.json():
-    print(repos['name'] + ' - ' + repos['html_url'])
+with open(input_username+"_repos_list.json", "w") as fp:
+    for repos in get_link.json():
+        json.dump(fp.writelines(repos['name'] + ' - ' + repos['html_url']+'\n'), fp)
+
+# with open(input_username+"_repos_list.json", "w") as fp:
+#     for repos in get_link.json():
+#         fp.writelines(repos['name'] + ' - ' + repos['html_url']+'\n')
