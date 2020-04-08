e11004c530b1133d3feb68a4828fcff60b9a4f53
roknikrol <roknikrol@gmail.com>
2020-04-08 17:39:05 +0300

    new issue

--- Lesson_7/MailBox_Selenium.py
+++ Lesson_7/MailBox_Selenium.py
@@ -10,7 +10,7 @@ from selenium.common.exceptions import StaleElementReferenceException
 import time
 #main selenium settings
 opera_options = Options()
-opera_options.add_argument('start-maximized')
+# opera_options.add_argument('start-maximized')
 driver = webdriver.Opera(options=opera_options)
 
 # get inside th mailbox
@@ -20,8 +20,9 @@ elem.send_keys('study.ai_172@mail.ru')
 elem.send_keys(Keys.RETURN)
 # button = WebDriverWait(elem, 5).until(
 #     EC.presence_of_element_located((By.CLASS_NAME,
-#                                     'btn btn_blue mailbox__control mailbox__control_twostep mailbox__rwd-control mailbox__control_twostep-short'))
+#                                     'mailbox__control_twostep-short'))
 # )
+time.sleep(1)
 elem = driver.find_element_by_id("mailbox:password")
 elem.send_keys('NewPassword172')
 elem.send_keys(Keys.RETURN)
@@ -32,22 +33,24 @@ email_dict = {}
 page = 1 #
 time.sleep(10)
 while page < 5: # for test purposes - only 5 pages
-    emails = driver.find_elements_by_class_name("ll-sj__normal")
+    emails = driver.find_elements_by_class_name("llc__subject")
     actions = ActionChains(driver)
     for email in emails:
         email.click()
         # button = WebDriverWait(driver, 6).until(email.find_element_by_xpath("//div/h2[@class='thread__subject']"))
-        # time.sleep(15)
-        print(email.find_element_by_xpath("//div/h2[@class='thread__subject']/text()")) # ЗДЕСЬ ВЫХОДИТ ОШИБКА StaleElementReferenceException
+        time.sleep(5)
+        print(driver.find_element_by_xpath("//div/h2[@class='thread__subject']").text)
         # email_dict['header'] = email.find_element_by_xpath("//div/h2[@class='thread__subject']/text()")
         # email_dict['from'] = email.find_element_by_class_name("letter-contact letter-contact_active").value_of_css_property("title").text
         # email_dict['when'] = email.find_element_by_class_name("letter__date").text
         # email_dict['body'] = email.find_element_by_class_name("webkit_mailru_css_attribute_postfix").text
-        emails_data.append(email_dict)
-        email.back()
+        # emails_data.append(email_dict)
+        back = driver.find_element_by_class_name("ico_16-arrow-back")
+        back.click()
+        time.sleep(5)
     actions.move_to_element(emails[-1])
     actions.perform()
     page += 1
-print(emails_data)
+# print(emails_data)
 
 # driver.quit()
